bb7b41ef751b686b2445bfe5a543383d404adfd2
Yeasir0995 <40072145+Yeasir0995@users.noreply.github.com>
2020-07-20 16:08:39 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameCode
+{
+    class GameCode
+    {
+        static void Main(string[] args)
+        {
+            int totalSecondsPlayed = 120;
+            const int SecondsPerMinute = 60;
+            int minutesPlayed = totalSecondsPlayed / SecondsPerMinute;
+            int secondsPlayed = totalSecondsPlayed % SecondsPerMinute;
+            {
+                Console.WriteLine("Minutes Played: " + minutesPlayed);
+                Console.WriteLine("Seconds Played: " + secondsPlayed);
+            }
+            
+        }
+    }
+}
