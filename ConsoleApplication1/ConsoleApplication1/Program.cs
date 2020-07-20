2858c98b6c4972af1ebcc83f3176a992c5106d8e
Yeasir0995 <40072145+Yeasir0995@users.noreply.github.com>
2020-07-20 16:18:21 +0600

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    struct User
+    {
+        public string name;
+        public string location;
+        int age;
+        public User(string name, string location, int age)
+        {
+            this.name = name;
+            this.location = location;
+            this.age = age;
+
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                User u = new ConsoleApplication1.User("raju","Uttarkhand", 32);
+                Console.WriteLine("Name:{0}, Location:{1}, Age:{2}", u.name, u.location, u.age);
+            }
+        }
+    }
+}
