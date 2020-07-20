2c8b2e5a0a2254dd3d1be36ebe5771f21094a1cf
Yeasir0995 <40072145+Yeasir0995@users.noreply.github.com>
2020-07-20 16:23:14 +0600

    Add files via upload

---
+++ Y001/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Y001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ourString;
+            ourString = Console.ReadLine();
+            Console.WriteLine(ourString);
+            int z,h;
+            double p;
+
+            ourString = Console.ReadLine();
+            z = Convert.ToInt32(Console.ReadLine());
+            p = Convert.ToDouble(Console.ReadLine());
+            h = Int32.Parse(Console.ReadLine());
+            // p = Console.ReadLine();
+            int n1, n2;
+            double p1, p2;
+            short b1, b2;
+
+
+
+
+
+        }
+    }
+}
