0fd90803d4dc1d0e4d3990d4b9cd92484902e347
Yeasir0995 <40072145+Yeasir0995@users.noreply.github.com>
2020-07-20 16:28:27 +0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,31 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Y001
+namespace Y003
 {
+    struct Point
+    {
+        public int x;
+        public int y;
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            string ourString;
-            ourString = Console.ReadLine();
-            Console.WriteLine(ourString);
-            int z,h;
-            double p;
-
-            ourString = Console.ReadLine();
-            z = Convert.ToInt32(Console.ReadLine());
-            p = Convert.ToDouble(Console.ReadLine());
-            h = Int32.Parse(Console.ReadLine());
-            // p = Console.ReadLine();
-            int n1, n2;
-            double p1, p2;
-            short b1, b2;
-
-
-
-
-
+            /* Point p1, p2;
+             p1.x = 20;
+             p1.y = 30;
+             Console.WriteLine("{0},{1}", p1.x, p1.y);
+             p2 = p1;
+             p1.x++;
+             */
+            int p,pp;
+            double g,gg;
+            p = Convert.ToInt32(Console.ReadLine());
+            pp = Int32.Parse(Console.ReadLine());
+            g = Convert.ToDouble(Console.ReadLine());
+            gg = Double.Parse(Console.ReadLine());
+            //type_name var_name[= init];
+            int n1, n2 = 11;
+            double n3 = 5, n4;
+            short n5 = 10, n6;
+            n1 = n5;
+            n4 = n2;
+            n1 = (int)n3;
+            n6 = (short)n2;
         }
     }
 }
