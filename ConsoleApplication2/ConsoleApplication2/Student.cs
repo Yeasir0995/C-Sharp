0e7d788bb5d4ae5db085f379f8d0a4d7d53960a1
Yeasir0995 <40072145+Yeasir0995@users.noreply.github.com>
2020-07-20 16:19:50 +0600

    Add files via upload

---
+++ ConsoleApplication2/ConsoleApplication2/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Student
+{
+    byte id;
+    string name;
+    double cgpa;
+    string bloodGroup;
+    public Student
+        {
+        }
+
+    public Student(byte id,string name,double cgpa,string bloodGroup)
+	{
+        this.id = id;
+        this.name = name;
+        this.cgpa = cgpa;
+        this.bloodGroup = bloodGroup;
+
+       
+	}
+    public void ShowStudentInfo()
+    {
+
+    }
+}
